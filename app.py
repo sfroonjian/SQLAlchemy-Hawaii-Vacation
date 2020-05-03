cf80a812667cd892da1586dd821c0e53aac896d4
Sandy <60555203+sfroonjian@users.noreply.github.com>
2020-05-03 14:28:27 -0400

    annoted all code and made some parts more efficient

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+# import dependencies
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 
 #database setup
@@ -38,13 +37,16 @@ def home():
     )
 
 # precipiation page
+# retreive the average prcp every day for the past 12 months and order by the date
 @app.route("/api/v1.0/precipitation")
 def precipiation():
-    # Create our session (link) from Python to the DB
     session = Session(engine)
-    precipitation = session.query(Measurement.date, Measurement.prcp).all()
-    session.close()
-
+    precipitation = session.query(Measurement.date, func.avg(Measurement.prcp)).\
+        filter(Measurement.date > "2016-08-23").\
+        group_by(Measurement.date).\
+        order_by(Measurement.date).all()
+    
+    # add all data into a list to be jsonified
     precipitation_list = []
     for date, prcp in precipitation:
         precipitation_dict = {}
@@ -62,6 +64,7 @@ def station():
     station = session.query(Station.station).all()
     session.close()
 
+    # make the format of data jsonifyable
     all_stations = list(np.ravel(station))
     return jsonify(all_stations)
 
@@ -69,15 +72,18 @@ def station():
 @app.route("/api/v1.0/tobs")
 def tobs():
     session = Session(engine)
+    # find which station is the most active/has the most TOBs
     most_active_station = session.query(Measurement.station).\
         group_by(Measurement.station).\
         order_by(func.count(Measurement.station).desc()).first()
+    # find TOB per day for the past 12 months for the most active station
     tob_12month = session.query(Measurement.date, Measurement.tobs).\
         filter(Measurement.station==most_active_station[0]).\
         filter(Measurement.date > '2016-08-18').\
         order_by(Measurement.date.desc()).all()
     session.close()
 
+    # add all data into a list to be jsonified
     tobs_list = []
     for date, tobs in tob_12month:
         tob_12month_dict = {}
@@ -91,10 +97,12 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 def start_normals(start):
     session = Session(engine)
+    # finds the temp min, max, and average for each day after and including the date the user enters
     temp_normals_start = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).all()
     session.close()
 
+    # add all data into a list to be jsonified
     start_list = []
     for tmin, tavg, tmax in temp_normals_start:
         start_list_dict = {}
@@ -109,10 +117,12 @@ def start_normals(start):
 @app.route("/api/v1.0/<start>/<end>")
 def startend_normals(start, end):
     session = Session(engine)
+    # finds the temp min, max, and average for each day in between and including the dates the user enters
     temp_normals_startend = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).filter(Measurement.date <= end).all()
     session.close()
 
+    # add all data into a list to be jsonified
     startend_list = []
     for tmin, tavg, tmax in temp_normals_startend:
         startend_list_dict = {}
@@ -123,6 +133,6 @@ def startend_normals(start, end):
 
     return jsonify(startend_list)
 
-
+# allows you to run the server from the command line with "python app.py"
 if __name__ == '__main__':
     app.run(debug=True)
